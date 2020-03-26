5b8f48b6c94de63c3980825e1bf0e6ed2c056529
Vitória Souza <vitoria.souza@cupula.local>
2020-03-26 20:22:11 -0300

    structure of login page

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+import "./styles.css";
+import heroesImg from "../../assets/heroes.png";
+import logoImg from "../../assets/logo.svg";
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+          <FiLogIn size={16} color="#E02041" />
+          <a href="/register"> Não tenho cadastro </a>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
+}
