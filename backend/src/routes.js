871ac241b74453f226cd4e02b3504f768149f1c6
Vitória Souza <vitoria.souza@cupula.local>
2020-03-25 21:19:58 -0300

    ong creation and listing

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const routes = express.Router();
+const OngController = require("./controllers/OngController");
 
-routes.get("/users", (request, response) => {
-  return response.json({
-    evento: "Semana OmniStack 11.0",
-    aluno: "Vitória Souza"
-  });
-});
+routes.get("/ongs", OngController.index);
+
+routes.post("/ongs", OngController.create);
 
 module.exports = routes;
