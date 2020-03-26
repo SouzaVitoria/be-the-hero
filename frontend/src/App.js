3ec7d48b7c664688496c724446515e5a6a2765ce
Vitória Souza <vitoria.souza@cupula.local>
2020-03-26 19:35:05 -0300

    basic concept of state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 function App() {
-  return <Header title="Semana OmniStack" />;
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <Header>Count: {counter}</Header>
+      <button onClick={increment}>Count</button>
+    </div>
+  );
 }
 
 export default App;
