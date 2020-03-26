1f2809dc1899af0f373e59980a36fdc5666ab340
Vitória Souza <vitoria.souza@cupula.local>
2020-03-26 19:55:21 -0300

    update files and style global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+import "./global.css";
+import Logon from "./pages/Logon";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
     <div>
-      <Header>Count: {counter}</Header>
-      <button onClick={increment}>Count</button>
+      <Logon />
     </div>
   );
 }
